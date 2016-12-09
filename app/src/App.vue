d895fcb5342adf3b76e2dbbe0a2edf6beb896e64
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-12-08 19:39:23 -0500

    First commit

---
+++ app/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import store from 'src/vuex/store'
+
+  export default {
+    store
+  }
+</script>
---
+++ app/src/routes.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    path: '/',
+    name: 'dash',
+    component: require('./views/Dash.vue')
+  }
+]
